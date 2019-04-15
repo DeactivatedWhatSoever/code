c4c5ee110d217b55261f1fe14d9f8615c1bc6161
RobinRheem <roborheems@gmail.com>
2019-04-15 10:44:45 +0900

    C++ function declaration and definition

---
+++ cpp/functions_declared_defined.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Declaration and definition
+int DemoConsoleOutput()
+{
+  cout << "This is a simple string literal" << endl;
+  cout << "Writing number five: " << 5 << endl;
+  cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
+  cout << "Pi when aproximated is 22 / 7 = " << 22 / 7 << endl;
+  cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
+  return 0;
+}
+
+int main()
+{
+  return DemoConsoleOutput();
+}
+
+
