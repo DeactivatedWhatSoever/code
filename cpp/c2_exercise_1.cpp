8d504c8753c1968c437ac782272c06ff805f2d3b
RobinRheem <roborheems@gmail.com>
2019-04-17 11:07:14 +0900

    C++ a Day C2 Exercises

---
+++ cpp/c2_exercise_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  // The exercise was nothing. Just putting out the double quote,
+  // and making the return type void in the main function
+  std::cout << "Is there a bug here?";  
+  return 0;
+}
+
---
+++ cpp/c2_exercise_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function declare, but the naming is a class! Is it right?
+int DemoConsoleOutput();
+
+int main()
+{
+  DemoConsoleOutput();
+  return 0;
+}
+
+// Implementation of declared function
+int DemoConsoleOutput()
+{
+  cout << "This is a simple string literal" << endl;
+  cout << "Writing number five: " << 5 << endl;
+  cout << "Subtraction: 4 - 3 = " << 4 - 3 << endl;
+  cout << "Performing multiplication 10 * 5 = " << 10 * 5 << endl;
+  cout << "Pi when aproximated is 22 / 7 = " << 22 / 7 << endl;
+  cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
+  return 0;
+}
+
