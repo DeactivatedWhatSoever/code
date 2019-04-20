f52cf3f76f6b03bffea0102247ecc1d2e5b1f170
RobinRheem <roborheems@gmail.com>
2019-04-20 15:43:30 +0900

    C++ global variables

---
+++ cpp/global_variables.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int firstNumber = 0;
+int secondNumber = 0;
+int multiplicationResult = 0;
+
+void MultiplyNumbers() 
+{
+  cout << "Enter the first number: ";
+  cin >> firstNumber;
+
+  cout << "Enter the second number: ";
+  cin >> secondNumber;
+
+  multiplicationResult = firstNumber * secondNumber;
+
+  cout << "Displaying from MultiplyNumbers(): ";
+  cout << firstNumber << " x " << secondNumber;
+  cout << " = " << multiplicationResult << endl;
+}
+
+int main()
+{
+  cout << "This program will help you multiply two numbers" << endl;
+  MultiplyNumbers();
+
+  cout << "Displaying from main(): ";
+  cout << firstNumber << " x " << secondNumber;
+  cout << " = " << multiplicationResult << endl;
+  return 0;
+}
+
