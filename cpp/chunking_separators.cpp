a3a9bf055dbccc6cdf21aad449fbd5074ef3a638
RobinRheem <roborheems@gmail.com>
2019-04-22 17:48:24 +0900

    C++ chunking separators

---
+++ cpp/chunking_separators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // It's all about the '
+  // It's a good thing for separating!
+  // More of a reading enhancement
+  int moneyInBank = 70'000;
+  long populationChange = -85'000;
+  long long countryGDPChange = 70'000'000'000;
+  double pi = 3.141'592'653'59;
+
+  cout << moneyInBank << endl;
+  cout << populationChange << endl;
+  cout << countryGDPChange << endl;
+  cout << pi << endl;
+  return 0;
+}
+
