da75aeba95dac706e26cbb68a4b2acc5cd0d6ae7
RobinRheem <roborheems@gmail.com>
2019-04-25 07:39:37 +0900

    C++ const

---
+++ cpp/const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+
+  const double pi = 22.0 / 7;
+  cout << "The value of constant pi is: " << pi << endl;
+  // Check to see if compile fails
+  //pi = 345;
+  return 0;
+}
