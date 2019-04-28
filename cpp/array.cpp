5340457e3da9f621e457261060c623c8a90d4221
RobinRheem <roborheems@gmail.com>
2019-04-28 20:39:14 +0900

    C++ array

---
+++ cpp/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int myNumbers [5] = {1, 2, 3, 4, 5};
+
+  cout << "First element at index 0: " << myNumbers [0] << endl;
+  cout << "Second element at index 1: " << myNumbers [1] << endl;
+  cout << "Third element at index 2: " << myNumbers [2] << endl;
+  cout << "Fourth element at index 3: " << myNumbers [3] << endl;
+  cout << "Fifth element at index 0: " << myNumbers [4] << endl;
+
+  return 0;
+}
+
